4b3f00881d6d84e124542360172e0dcd29845768
Jack Mills <Jack_mills_90@yahoo.com>
2014-02-18 14:39:27 +0000

    Version 0.1

--- JavaScript/Map.js
+++ JavaScript/Map.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+
+var mapOptions
+var mapDiv = document.getElementById("map");
+var cheltenham = new google.maps.LatLng(51.897476 , -2.077575);
+
+google.maps.event.addDomListener(window, "load", start);
+	
+function start(){
+	
+	var map = new mapp(cheltenham);
+	
+	}
+
+function mapp(location){
+	
+	mapOptions = {
+		center: location,
+		minZoom: 12,
+		zoom: 12
+	}
+	
+	var googleMap = new google.maps.Map(mapDiv,mapOptions);
+
+	}
